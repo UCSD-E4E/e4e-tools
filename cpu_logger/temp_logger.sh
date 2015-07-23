197500a020e5af378750602cc68f9452ef94a710
ntlhui <ntlhui@gmail.com>
2015-07-23 15:34:12 -0700

    Added temperature logger

---
+++ cpu_logger/temp_logger.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+log="tmp_log.log"
+if [ "$#" -eq "1" ]
+then
+	log=$1
+fi
+while [ 1 ]
+do
+	data=`cat /sys/class/hwmon/hwmon0/temp1_input`
+	time=`date +%s`
+	echo "$time $data" >> $log
+	sleep 3
+done
