17178a46f22715b6fdc0a1600aed30e89f41257b
ntlhui <ntlhui@gmail.com>
2015-08-10 16:49:55 -0700

    Fixed temp_logger for BBB

--- cpu_logger/temp_logger.sh
+++ cpu_logger/temp_logger.sh
@@ -6,7 +6,7 @@ then
 fi
 while [ 1 ]
 do
-	data=`cat /sys/class/hwmon/hwmon0/temp1_input`
+	data=`cat /sys/class/hwmon/hwmon0/device/temp1_input`
 	time=`date +%s`
 	echo "$time $data" >> $log
 	# Record period below
