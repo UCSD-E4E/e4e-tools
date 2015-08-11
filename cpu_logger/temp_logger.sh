cbcd5305f75c4e3736a05a7ee19ee1a2dd6db10d
ntlhui <ntlhui@gmail.com>
2015-08-10 17:33:00 -0700

    Added line for raspberry

--- cpu_logger/temp_logger.sh
+++ cpu_logger/temp_logger.sh
@@ -7,6 +7,7 @@ fi
 while [ 1 ]
 do
 	data=`cat /sys/class/hwmon/hwmon0/device/temp1_input`
+	#data=`cat /sys/class/thermal/thermal_zon0/temp`
 	time=`date +%s`
 	echo "$time $data" >> $log
 	# Record period below
