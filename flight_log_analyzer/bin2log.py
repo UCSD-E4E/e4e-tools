225d882734f456ddcc5f00d4e1a9fb11af058972
Nathan Hui <ntlhui@gmail.com>
2017-03-03 05:24:07 -0800

    Implemented, minor issues with field format remaining

--- flight_log_analyzer/bin2log.py
+++ flight_log_analyzer/bin2log.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 
+from pymavlink import mavutil
+
 def bin2log(bin_filename, log_filename):
-	binfile = open(bin_filename, 'rb')
+	mavmaster = mavutil.mavlink_connection(bin_filename)
 	logfile = open(log_filename, 'w')
+	while True:
+		msg = mavmaster.recv_match()
+		if msg is None:
+			break
+		logfile.write('%s, '% msg.get_type())
+		for field in xrange(len(msg.get_fieldnames()) - 1):
+			logfile.write('%s, '% msg.to_dict()[msg.get_fieldnames()[field]])
+		logfile.write('%s\n'% msg.to_dict()[msg.get_fieldnames()[len(msg.get_fieldnames()) - 1]])
+
 
 def main():
-	bin_filename = '8.BIN'
-	log_filename = '8.log'
+	bin_filename = '5.BIN'
+	log_filename = '5.log'
 	bin2log(bin_filename, log_filename)
 
 if __name__ == '__main__':
